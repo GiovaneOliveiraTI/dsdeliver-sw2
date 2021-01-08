23f7bbfe953d712a489c9eb998885f905b98ce28
Giovane <giovaneoliveira_ti@hotmail.com>
2021-01-08 10:05:29 -0300

    criando layout e consumindo apis com a listas de produtos

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,24 @@
 import App from "../App"
-
 import './styles.css';
 import {ReactComponent as Logo } from './logo.svg';
 import StepsHeader from "./StepsHeader";
+import ProductList from "./ProductList";
+import { useEffect, useState } from "react";
+import { Product } from "./types";
+import { fetchProducts } from "./api";
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]); 
+    console.log(products);
+    useEffect(() =>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error));
+    }, []);
+
     return (
      <div className="orders-container">
          <StepsHeader />
+         <ProductList  products ={products} />    
      </div>
     )
 }
