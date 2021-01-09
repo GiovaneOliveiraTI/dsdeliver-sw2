e163f594b297c41a7a75d224db851f67309cbdd7
Giovane <giovaneoliveira_ti@hotmail.com>
2021-01-09 11:46:27 -0300

    Correção componente StepsHeader.tsx.

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -5,7 +5,7 @@ function StepsHeader() {
         <h1 className="steps-title">
             SIGA AS <br /> ETAPAS
         </h1>
-        <ul className="steps-itens">
+        <ul className="steps-items">
          <li>
              <span className="steps-number">1</span>
              Selecione os produtos e localização.
